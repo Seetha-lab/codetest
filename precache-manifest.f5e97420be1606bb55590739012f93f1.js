8146c54cc00f6fe19fa62a2df73bd5f71044228a
Ayyappan <ayyapanr@hotmail.com>
2020-07-01 17:47:35 +1000

    Updates

---
+++ precache-manifest.f5e97420be1606bb55590739012f93f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2d53cad21beb65aa5d4681b0efc0a9",
+    "url": "/codetest/index.html"
+  },
+  {
+    "revision": "f4d702e537395dd2b5e7",
+    "url": "/codetest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f4b7558498a00d8ec34",
+    "url": "/codetest/static/js/2.87443482.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codetest/static/js/2.87443482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d702e537395dd2b5e7",
+    "url": "/codetest/static/js/main.34a712be.chunk.js"
+  },
+  {
+    "revision": "9ba53c8011033ad64bb2",
+    "url": "/codetest/static/js/runtime-main.724e0ea6.js"
+  }
+]);
